34119b91a5cadecf064bda42a4579d10a12fadbc
xxx0527 <3466392059@qq.com>
2019-03-23 17:28:38 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="container">
+    <x-header :right-options="{showMore: true}" :left-options="{showBack: false}">我爱读书</x-header>
+    <swiper loop auto :list="img_list" :index="img_index"></swiper>
+    <grid :cols="4" :show-lr-borders="false">
+      <grid-item :label="('Grid')" v-for="i in 8" :key="i">
+        <img slot="icon" src="../assets/grid.png">
+      </grid-item>
+    </grid>
+    <panel :header="'推荐'" :footer="footer" :list="list" :type="type"></panel>
+    <tabbar>
+      <tabbar-item>
+        <img slot="icon" src="../assets/home.png">
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/book.png">
+        <span slot="label">书籍</span>
+      </tabbar-item>
+      <tabbar-item selected link="/component/demo">
+        <img slot="icon" src="../assets/map.png">
+        <span slot="label">附近</span>
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/mine.png">
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell, XHeader, Grid, GridItem, Swiper, Panel } from 'vux'
+
+const baseList = [{
+  url: 'javascript:',
+  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
+  title: '送你一朵fua'
+}, {
+  url: 'javascript:',
+  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw1k2wj20p00goq7n.jpg',
+  title: '送你一辆车'
+}, {
+  url: 'javascript:',
+  img: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw1k2wj20p00goq7n.jpg',
+  title: '送你一次旅行',
+  fallbackImg: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vw50iwj20ff0aaaci.jpg'
+}]
+
+const urlList = baseList.map((item, index) => ({
+  url: 'http://m.baidu.com',
+  img: item.img,
+  fallbackImg: item.fallbackImg,
+  title: `(可点击)${item.title}`
+}))
+
+export default {
+  components: {
+    Panel,
+    XHeader,
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell,
+    Grid,
+    GridItem,
+    Swiper
+  },
+  data () {
+    return {
+      img_list: urlList,
+      img_index: 0,
+      msg: 'Hello World!',
+      type: '1',
+      list: [{
+        src: 'http://www.zhantengwang.com/uploads/allimg/181016/1-1Q016144920196.jpg',
+        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: '/component/cell'
+      }, {
+        src: 'http://www.zhantengwang.com/uploads/allimg/181016/1-1Q016144920196.jpg',
+        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: '/component/cell'
+      }, {
+        src: 'http://www.zhantengwang.com/uploads/allimg/181016/1-1Q016144920196.jpg',
+        fallbackSrc: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: '/component/cell'
+      }, {
+        src: 'http://www.zhantengwang.com/uploads/allimg/181016/1-1Q016144920196.jpg',
+        title: '标题二',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+        url: {
+          path: '/component/radio',
+          replace: false
+        },
+        meta: {
+          source: '来源信息',
+          date: '时间',
+          other: '其他信息'
+        }
+      }],
+      footer: {
+        title: '更多',
+        url: 'http://vux.li'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container /deep/ .weui-tabbar {
+  position: fixed;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="container">
+    <x-header :right-options="{showMore: False}"  :left-options="{showBack: ture}">Login</x-header>
+  <x-input title="手机号" mask="999 9999 9999"  :max="13"   is-type="china-mobile"></x-input>
+  <x-input title="密码" :max="13" type="password" v-model="password" ></x-input>
+  <x-button @click.native="Login">登录</x-button>
+</div>
+</template>
+
+<script>
+import { XButton, XInput, XHeader, AlertModule, Alert } from 'vux'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      password: '',
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    Login () {
+      if (this.password === '123456') {
+        this.$router.push('/')
+      } else {
+        AlertModule.show({
+          title: '登录失败',
+          content: '请重新登陆！'
+        })
+      }
+    }
+  },
+  HelloFromVux: {
+    goBack () {
+      this.$router.push('/HelloFromVux')
+    }
+  },
+  components: {
+    XButton,
+    XInput,
+    XHeader,
+    Alert
+  }
+}
+</script>
+
+  
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
